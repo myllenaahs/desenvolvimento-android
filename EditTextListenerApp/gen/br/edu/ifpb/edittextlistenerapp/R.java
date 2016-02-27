6fde373b7d1e5d216a0aceff96972b35eee3718d
Myllena <Denize@192.168.25.102>
2016-02-27 08:38:58 -0300

    Activity de Splash

--- EditTextListenerApp/gen/br/edu/ifpb/edittextlistenerapp/R.java
+++ EditTextListenerApp/gen/br/edu/ifpb/edittextlistenerapp/R.java
@@ -12,18 +12,24 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int logo=0x7f020001;
+        public static final int wallpaper=0x7f020002;
     }
     public static final class id {
+        public static final int imageView1=0x7f060003;
         public static final int linearLayout1=0x7f060000;
         public static final int nome=0x7f060001;
         public static final int nomesListView=0x7f060002;
+        public static final int progressBar1=0x7f060004;
     }
     public static final class layout {
         public static final int activity_buscar_nomes=0x7f030000;
+        public static final int activity_splash=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
+        public static final int title_activity_splash=0x7f040002;
     }
     public static final class style {
         /** 
